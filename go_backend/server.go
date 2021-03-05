7a0b90d4c307219b90625a79c798263757ca96ad
sz-sebestyen <szabosebestyen.mail@gmail.com>
2021-03-05 23:22:53 +0100

    Fix static file serving

--- go_backend/server.go
+++ go_backend/server.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
-
-// PORT is what the server is listening on
-const PORT = 8080
 
 func main() {
 
 	fs := http.FileServer(http.Dir("../react_frontend/build/"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on :" + fmt.Sprint(PORT) + "...")
-	err := http.ListenAndServe(":"+fmt.Sprint(PORT), nil)
+	log.Println("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
