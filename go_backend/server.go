b71038491a7a90dc4c7ad5a5e9b301634562e41d
sz-sebestyen <szabosebestyen.mail@gmail.com>
2021-03-05 23:09:26 +0100

    Server react app

--- go_backend/server.go
+++ go_backend/server.go
@@ -2,40 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 // PORT is what the server is listening on
 const PORT = 8080
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
-}
-
 func main() {
 
-	router := mux.NewRouter()
+	fs := http.FileServer(http.Dir("../react_frontend/build/"))
+	http.Handle("/", fs)
 
-	router.HandleFunc("/{something}", SomethingHandler)
-
-	http.Handle("/", router)
-
-	fmt.Println("Server is running on PORT " + fmt.Sprint(PORT))
-
-	if err := http.ListenAndServe(":"+fmt.Sprint(PORT), nil); err != nil {
-		panic(err)
+	log.Println("Listening on :" + fmt.Sprint(PORT) + "...")
+	err := http.ListenAndServe(":"+fmt.Sprint(PORT), nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-}
-
-// SomethingHandler responds to route /{something}
-func SomethingHandler(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-
-	w.WriteHeader(http.StatusOK)
-
-	w.Write([]byte(vars["something"]))
-
-	// fmt.Fprintf(w, "Something: %v\n", vars["something"])
 }
