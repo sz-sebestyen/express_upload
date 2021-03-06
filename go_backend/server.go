1b7198bfc88623dfdf6aa7ffdee562659c6f6a73
sz-sebestyen <szabosebestyen.mail@gmail.com>
2021-03-06 10:55:46 +0100

    Parse JSON data

--- go_backend/server.go
+++ go_backend/server.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 )
 
 func main() {
-
-	fs := http.FileServer(http.Dir("../react_frontend/build/"))
-	http.Handle("/", fs)
+	http.Handle("/", http.FileServer(http.Dir("../react_frontend/build/")))
 
 	http.HandleFunc("/upload", uploadHandler)
 
@@ -24,43 +24,48 @@ func main() {
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+
 		err := r.ParseMultipartForm(32 << 20) // 32MB max upload size
-		checkForError(err, w)
-
-		fmt.Print(r.MultipartForm)
-
-		fmt.Print("\n")
-
-		json := r.FormValue("jsonData")
-		fmt.Print(json)
-
-		files := r.MultipartForm.File["uploadFiles"]
-
-		for _, fileHeader := range files {
-			file, err := fileHeader.Open()
-
-			defer file.Close()
-			checkForError(err, w)
-
-			f, err := os.OpenFile("./upload/"+fileHeader.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-			checkForError(err, w)
-
-			defer f.Close()
-			io.Copy(f, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "application/json")
+		var parsedJSON map[string]interface{}
+		json.Unmarshal([]byte(r.FormValue("jsonData")), &parsedJSON)
+
+		fmt.Print(r.MultipartForm.File, "\n", parsedJSON, "\n")
+
+		for _, fileHeader := range r.MultipartForm.File["uploadFiles"] {
+			if saveFile(fileHeader) != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
 		w.Write([]byte("success"))
+	} else {
+		w.WriteHeader(400)
+		// w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("error"))
 	}
-	// w.Header().Set("Content-Type", "application/json")
-	// w.Write([]byte("error"))
-	// w.WriteHeader(400)
 }
 
-func checkForError(err error, w http.ResponseWriter) {
+func saveFile(fileHeader *multipart.FileHeader) error {
+	file, err := fileHeader.Open()
+	defer file.Close()
+
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return err
 	}
+
+	savedFile, err := os.OpenFile("./upload/"+fileHeader.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	defer savedFile.Close()
+
+	if err != nil {
+		return err
+	}
+
+	io.Copy(savedFile, file)
+	return nil
 }
